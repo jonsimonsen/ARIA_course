ccf538feb6a11878f3366d630675482c887cd98e
Jon <jon@ramsalt.com>
2020-09-29 12:35:23 +0200

    Changed selectors to use role instead of class.

--- task1/script.js
+++ task1/script.js
@@ -1,6 +1,6 @@
 import { hide, show } from '../common/scripts/common.js';
 
-const tabs      = document.querySelectorAll('.tab');
+const tabs      = document.querySelectorAll('[role="tab"]');
 const tabPanels = document.querySelectorAll('.tabpanel');
 
 /**
